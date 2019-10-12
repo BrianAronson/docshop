4bd3fc6238aeb1a06300549bf81e7723537c0db5
Brian Aronson <bdaronson@gmail.com>
2019-10-12 01:36:45 -0400

    Add files via upload
    Fixed s3list.files to correctly return full file paths and to exclude folder names

--- R/s3list.files.R
+++ R/s3list.files.R
@@ -5,14 +5,22 @@
 s3.list.files<-function(x, full.names=F, max.files=1000){
   #1) make function for finding extracting bucket name (in case included in x)
       prefix.fun <- function(x) gsub("s3://iuni-doctorshopping/", "", x)
+      
   #2) find files in bucket
       bucket.path <- get_bucket("s3://iuni-doctorshopping/", max = max.files, prefix = prefix.fun(x))
+      
   #3) extract file names
       key.names <- as.vector(sapply(bucket.path, function(x) x[[1]]))
-  #4) create versions of file names including full path and excluding full path
-      full.return.names <- file.path("s3://iuni-doctorshopping/", key.names)
+      
+  #4) remove folder names from key.names
+      key.names <- key.names
+      key.names <- key.names[!grepl("/$", key.names)]
+      
+  #5) create versions of file names including full path and excluding full path
+      full.return.names <- file.path("s3://iuni-doctorshopping", key.names)
       short.return.names <- gsub("(.*/\\s*(.*$))", "\\2", key.names)
-  #5) return file names according to whether full.names set to TRUE
+      
+  #6) return file names according to whether full.names set to TRUE
       if(full.names){
            return(full.return.names)
       }else{
